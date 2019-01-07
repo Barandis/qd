80e13e0fe761a63018cdb04d082980bee182ebb6
Thomas Otterson <barandis1729@gmail.com>
2019-01-07 09:13:40 -0500

    fix(algebraic): Remove extraneous slash
    A slash was left over from writing license headers.

--- src/double/algebraic.rs
+++ src/double/algebraic.rs
@@ -3,7 +3,7 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-/use crate::basic::*;
+use crate::basic::*;
 use crate::double::DoubleDouble;
 use std::f64;
 
