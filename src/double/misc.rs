a785bb276f97b1801580d002326ad19d5f2110fb
Thomas Otterson <barandis1729@gmail.com>
2019-01-16 18:24:39 -0500

    feat(misc): Add classify and is_normal
    Okay, so THIS is supposed to be the last function addition.

--- src/double/misc.rs
+++ src/double/misc.rs
@@ -87,6 +87,30 @@ impl Double {
 // #region Number properties
 
 impl Double {
+    pub fn classify(self) -> FpCategory {
+        use std::num::FpCategory::*;
+
+        let c0 = self.0.classify();
+        let c1 = self.1.classify();
+
+        if c0 == Zero && c1 == Zero {
+            Zero
+        } else if c0 == Subnormal || c1 == Subnormal {
+            Subnormal
+        } else if c0 == Infinite || c1 == Infinite {
+            Infinite
+        } else if c0 == Nan || c1 == Nan {
+            Nan
+        } else {
+            Normal
+        }
+    }
+
+    #[inline]
+    pub fn is_normal(self) -> bool {
+        self.classify() == FpCategory::Normal
+    }
+
     #[inline]
     pub fn is_zero(self) -> bool {
         self.0 == 0.0
