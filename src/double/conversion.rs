3d0fcf3d4376613c30af6c7505cd35a382951c7f
Thomas Otterson <barandis1729@gmail.com>
2019-01-17 17:28:19 -0500

    feat(conv): Add From implementations for u64, i64

--- src/double/conversion.rs
+++ src/double/conversion.rs
@@ -29,16 +29,36 @@ fn from_float(n: f64) -> Double {
         return Double(n, 0.0);
     }
 
-    // TODO: This needs investigation. The idea of converting the number back and forth from being
-    // a string seems terribly inefficient, but that shouldn't be declared to be so until
-    // benchmarking is done.
+    // TODO: This needs investigation. It seems incorrect to double-convert a value, once to a
+    // string and then back again, but parsing a number as a string might be the most sensible way
+    // to do it in this particular case.
 
     // `unwrap` is safe because `n.to_string` will never return a string that can't be parsed into
     // a Double
     n.to_string().parse().unwrap()
 }
 
-macro_rules! from_impl {
+#[inline]
+fn split_u64(a: u64) -> (u32, u32) {
+    let x = (a >> 32) as u32;
+    let y = a as u32;
+    (x, y)
+}
+
+fn from_u64(a: u64) -> Double {
+    let (x, y) = split_u64(a);
+    Double::from_add(x as f64 * 2f64.powi(32), y as f64)
+}
+
+fn from_i64(a: i64) -> Double {
+    let sign = a.signum();
+    let a = a.abs() as u64;
+    let (x, y) = split_u64(a);
+    let d = Double::from_add(x as f64 * 2f64.powi(32), y as f64);
+    if sign == -1 { -d } else { d }
+}
+
+macro_rules! from_float_impl {
     ($($t:ty)*) => ($(
         impl From<($t, $t)> for Double {
             fn from((a, b): ($t, $t)) -> Double {
@@ -54,7 +74,48 @@ macro_rules! from_impl {
     )*);
 }
 
-from_impl! { i8 i16 i32 u8 u16 u32 f32 f64 }
+macro_rules! from_int_impl {
+    ($($t:ty)*) => ($(
+        impl From<($t, $t)> for Double {
+            fn from((a, b): ($t, $t)) -> Double {
+                Double(a.into(), b.into())
+            }
+        }
+
+        impl From<$t> for Double {
+            fn from(a: $t) -> Double {
+                Double(a.into(), 0.0)
+            }
+        }
+    )*);
+}
+
+from_float_impl! { f32 f64 }
+from_int_impl! { i8 u8 i16 u16 i32 u32 }
+
+impl From<(u64, u64)> for Double {
+    fn from((a, b): (u64, u64)) -> Double {
+        from_u64(a) + from_u64(b)
+    }
+}
+
+impl From<u64> for Double {
+    fn from(a: u64) -> Double {
+        from_u64(a)
+    }
+}
+
+impl From<(i64, i64)> for Double {
+    fn from((a, b): (i64, i64)) -> Double {
+        from_i64(a) + from_i64(b)
+    }
+}
+
+impl From<i64> for Double {
+    fn from(a: i64) -> Double {
+        from_i64(a)
+    }
+}
 
 impl From<&str> for Double {
     /// Converts a string representation of a number into a `Double`.
@@ -89,3 +150,22 @@ impl Double {
 }
 
 // #endregion
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn conv_from_u64() {
+        let a = 0x0123456789abcdefu64;
+        let d = dd!(a);
+        assert_eq!(format!("{}", a), format!("{}", d));
+    }
+
+    #[test]
+    fn conv_from_i64() {
+        let a = -0x0123456789abcdefi64;
+        let d = dd!(a);
+        assert_eq!(format!("{}", a), format!("{}", d));
+    }
+}
