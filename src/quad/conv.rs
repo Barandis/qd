6c87e14aa183b57b4ea2539e2db968ad6adc3912
Thomas Otterson <barandis1729@gmail.com>
2019-01-17 22:04:06 -0500

    feat(conv): Create naive from implementations
    These will change once parsing is complete for quads, but they'll serve now to get the ball rolling

--- src/quad/conv.rs
+++ src/quad/conv.rs
@@ -3,3 +3,42 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+use crate::quad::Quad;
+
+// #region From implementations
+
+macro_rules! from_impl {
+    ($($t:ty)*) => ($(
+        impl From<($t, $t, $t, $t)> for Quad {
+            #[inline]
+            fn from((a, b, c, d): ($t, $t, $t, $t)) -> Quad {
+                Quad(a.into(), b.into(), c.into(), d.into())
+            }
+        }
+
+        impl From<($t, $t, $t)> for Quad {
+            #[inline]
+            fn from((a, b, c): ($t, $t, $t)) -> Quad {
+                Quad(a.into(), b.into(), c.into(), 0.0)
+            }
+        }
+
+        impl From<($t, $t)> for Quad {
+            #[inline]
+            fn from((a, b): ($t, $t)) -> Quad {
+                Quad(a.into(), b.into(), 0.0, 0.0)
+            }
+        }
+
+        impl From<$t> for Quad {
+            #[inline]
+            fn from(a: $t) -> Quad {
+                Quad(a.into(), 0.0, 0.0, 0.0)
+            }
+        }
+    )*);
+}
+
+from_impl! { i8 u8 i16 u16 i32 u32 f32 f64 }
+
+// #endregion
