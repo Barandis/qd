d5119b60aa9388e8301504fa2cbb4b59f9d5fa7d
Thomas Otterson <barandis1729@gmail.com>
2019-01-21 01:11:35 -0500

    test(ldexp): Add tests for ldexp

--- src/double/alg/ldexp.rs
+++ src/double/alg/ldexp.rs
@@ -28,3 +28,31 @@ impl Double {
         Double(self.0 * factor, self.1 * factor)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn calc() {
+        assert_exact!(dd!(48), dd!(3).ldexp(4));
+        assert_close!(dd!(0.078125), dd!(5).ldexp(-6));
+        assert_close!(
+            dd!("4.2165742826631309245621820777801e8"),
+            Double::PI.ldexp(27)
+        );
+        assert_close!(
+            dd!("0.000020738844516441690333254146357366"),
+            Double::E.ldexp(-17)
+        );
+    }
+
+    #[test]
+    fn edge() {
+        assert_exact!(Double::NAN, Double::NAN.ldexp(5));
+        assert_exact!(Double::INFINITY, Double::INFINITY.ldexp(4));
+        assert_exact!(Double::INFINITY, Double::INFINITY.ldexp(-4));
+        assert_exact!(Double::NEG_INFINITY, Double::NEG_INFINITY.ldexp(3));
+        assert_exact!(Double::NEG_INFINITY, Double::NEG_INFINITY.ldexp(-3));
+    }
+}
--- src/quad/alg/ldexp.rs
+++ src/quad/alg/ldexp.rs
@@ -10,8 +10,10 @@ impl Quad {
     ///
     /// Though this is not an everyday operation, it is often used in more advanced mathematical
     /// calculations (including several within this library). Therefore an implementation that is
-    /// much more efficient than calculating it through multiplication and [`powi`](#method.powi) is
-    /// offered despite it not being part of the `f64` API.
+    /// much more efficient than calculating it through multiplication and [`powi`] is offered
+    /// despite it not being part of the `f64` API.
+    ///
+    /// [`powi`]: #method.powi
     ///
     /// # Examples
     /// ```
@@ -31,5 +33,33 @@ impl Quad {
             self.2 * factor,
             self.3 * factor,
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn calc() {
+        assert_exact!(qd!(48), qd!(3).ldexp(4));
+        assert_close!(qd!(0.078125), qd!(5).ldexp(-6));
+        assert_close!(
+            qd!("4.216574282663130924562182077780080660863911808152513230508318081e8"),
+            Quad::PI.ldexp(27)
+        );
+        assert_close!(
+            qd!("0.00002073884451644169033325414635736589430051610636672942790959905722"),
+            Quad::E.ldexp(-17)
+        );
+    }
+
+    #[test]
+    fn edge() {
+        assert_exact!(Quad::NAN, Quad::NAN.ldexp(5));
+        assert_exact!(Quad::INFINITY, Quad::INFINITY.ldexp(4));
+        assert_exact!(Quad::INFINITY, Quad::INFINITY.ldexp(-4));
+        assert_exact!(Quad::NEG_INFINITY, Quad::NEG_INFINITY.ldexp(3));
+        assert_exact!(Quad::NEG_INFINITY, Quad::NEG_INFINITY.ldexp(-3));
     }
 }
