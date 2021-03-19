2f32289139086d54c2383cfd9e9bdc0d09525efb
Thomas Otterson <barandis1729@gmail.com>
2021-03-19 07:35:41 -0400

    feat(arith): add a second Neg impl to Quad

--- src/quad/arith/neg.rs
+++ src/quad/arith/neg.rs
@@ -9,9 +9,50 @@ use std::ops::Neg;
 impl Neg for Quad {
     type Output = Quad;
 
+    /// Negates this `Quad`, producing a new `Quad`.
+    /// 
+    /// This implements the unary `-` operator for `Quad`s.
+    /// 
+    /// # Examples
+    /// ```
+    /// # #[macro_use] extern crate qd;
+    /// # use qd::Quad;
+    /// # fn main() {
+    /// let x = -Quad::PI;
+    /// let expected = qd!("-3.141592653589793238462643383279502884197169399375105820974944592");
+    /// 
+    /// let diff = (x - expected).abs();
+    /// assert!(x < qd!(1e-60));
+    /// # }
+    /// ```
     #[inline]
     fn neg(self) -> Quad {
         Quad(-self.0, -self.1, -self.2, -self.3)
+    }
+}
+
+impl Neg for &Quad {
+    type Output = Quad;
+
+    /// Negates a reference to this `Quad`, producing a new `Quad`.
+    /// 
+    /// This implements the unary `-` operator for references to `Quad`s.
+    /// 
+    /// # Examples
+    /// ```
+    /// # #[macro_use] extern crate qd;
+    /// # use qd::Quad;
+    /// # fn main() {
+    /// let x = -&Quad::PI;
+    /// let expected = qd!("-3.141592653589793238462643383279502884197169399375105820974944592");
+    /// 
+    /// let diff = (x - expected).abs();
+    /// assert!(x < qd!(1e-60));
+    /// # }
+    /// ```
+    #[inline]
+    fn neg(self) -> Quad {
+        Quad(-(*self).0, -(*self).1, -(*self).2, -(*self).3)
     }
 }
 
@@ -28,6 +69,18 @@ mod tests {
         assert_close!(
             qd!("-2.718281828459045235360287471352662497757247093699959574966967628"),
             -Quad::E
+        );
+    }
+
+    #[test]
+    fn neg_ref() {
+        assert_close!(
+            qd!("-3.141592653589793238462643383279502884197169399375105820974944592"),
+            -&Quad::PI
+        );
+        assert_close!(
+            qd!("-2.718281828459045235360287471352662497757247093699959574966967628"),
+            -&Quad::E
         );
     }
 
