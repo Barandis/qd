2f52ae96faa3de37b077f889a597b061027f3bd3
Thomas Otterson <barandis1729@gmail.com>
2021-04-02 10:27:44 -0400

    test(iter): improve tests for quad iterative functions

--- src/quad/iter.rs
+++ src/quad/iter.rs
@@ -102,101 +102,77 @@ impl<'a> Product<&'a Quad> for Quad {
 mod tests {
     use super::*;
 
-    #[test]
-    fn sum() {
-        let actual: Quad = [qd!(1), qd!(2), qd!(3), qd!(4), qd!(5)].iter().sum();
-        assert_exact!(qd!(15), actual);
+    // sum tests
+    test_all_near!(
+        sum_nums_pi_234:
+            qd!("3.4033920413889426750011969985527947912136001826563646393895233083321"),
+            vec![Quad::FRAC_PI_2, Quad::FRAC_PI_3, Quad::FRAC_PI_4]
+                .into_iter()
+                .sum::<Quad>();
+        sum_refs_pi_234:
+            qd!("3.4033920413889426750011969985527947912136001826563646393895233083321"),
+            vec![Quad::FRAC_PI_2, Quad::FRAC_PI_3, Quad::FRAC_PI_4]
+                .iter()
+                .sum::<Quad>();
+    );
+    test_all_exact!(
+        sum_nums_15:
+            qd!(15),
+            vec![qd!(1), qd!(2), qd!(3), qd!(4), qd!(5)].into_iter().sum::<Quad>();
+        sum_refs_15:
+            qd!(15),
+            vec![qd!(1), qd!(2), qd!(3), qd!(4), qd!(5)].iter().sum::<Quad>();
+        sum_empty:
+            Quad::ZERO,
+            vec![].iter().sum::<Quad>();
+        sum_inf:
+            Quad::INFINITY,
+            vec![qd!(1), Quad::INFINITY, qd!(3)].iter().sum::<Quad>();
+        sum_neg_inf:
+            Quad::NEG_INFINITY,
+            vec![qd!(1), Quad::NEG_INFINITY, qd!(3)].iter().sum::<Quad>();
+        sum_both_inf:
+            Quad::NAN,
+            vec![Quad::INFINITY, Quad::NEG_INFINITY].iter().sum::<Quad>();
+        sum_nan:
+            Quad::NAN,
+            vec![qd!(1), qd!(2), Quad::NAN].iter().sum::<Quad>();
+    );
 
-        let actual: Quad = vec![Quad::FRAC_PI_2, Quad::FRAC_PI_3, Quad::FRAC_PI_4]
-            .into_iter()
-            .sum();
-        assert_close!(
-            qd!("3.403392041388942675001196998552794791213600182656364639389523308"),
-            actual
-        );
-
-        let actual: Quad = [qd!(1), qd!(2), qd!(3), qd!(4), qd!(5)].iter().sum();
-        assert_exact!(qd!(15), actual);
-
-        let actual: Quad = vec![Quad::FRAC_PI_2, Quad::FRAC_PI_3, Quad::FRAC_PI_4]
-            .iter()
-            .sum();
-        assert_close!(
-            qd!("3.403392041388942675001196998552794791213600182656364639389523308"),
-            actual
-        );
-    }
-
-    #[test]
-    fn sum_empty() {
-        let actual: Quad = vec![].iter().sum();
-        assert_exact!(Quad::ZERO, actual);
-    }
-
-    #[test]
-    fn sum_inf() {
-        let actual: Quad = vec![qd!(1), Quad::INFINITY, qd!(3)].iter().sum();
-        assert_exact!(Quad::INFINITY, actual);
-
-        let actual: Quad = vec![qd!(1), Quad::NEG_INFINITY, qd!(3)].iter().sum();
-        assert_exact!(Quad::NEG_INFINITY, actual);
-
-        let actual: Quad = vec![Quad::INFINITY, Quad::NEG_INFINITY].iter().sum();
-        assert_exact!(Quad::NAN, actual);
-    }
-
-    #[test]
-    fn sum_nan() {
-        let actual: Quad = vec![qd!(1), qd!(2), Quad::NAN].iter().sum();
-        assert_exact!(Quad::NAN, actual);
-    }
-
-    #[test]
-    fn product() {
-        let actual: Quad = [qd!(1), qd!(2), qd!(3), qd!(4), qd!(5)].iter().product();
-        assert_exact!(qd!(120), actual);
-
-        let actual: Quad = vec![Quad::FRAC_PI_2, Quad::FRAC_PI_3, Quad::FRAC_PI_4]
-            .into_iter()
-            .product();
-        assert_close!(
-            qd!("1.291928195012492507311513127795891466759387023578546153922689088"),
-            actual
-        );
-
-        let actual: Quad = [qd!(1), qd!(2), qd!(3), qd!(4), qd!(5)].iter().product();
-        assert_exact!(qd!(120), actual);
-
-        let actual: Quad = vec![Quad::FRAC_PI_2, Quad::FRAC_PI_3, Quad::FRAC_PI_4]
-            .iter()
-            .product();
-        assert_close!(
-            qd!("1.291928195012492507311513127795891466759387023578546153922689088"),
-            actual
-        );
-    }
-
-    #[test]
-    fn product_empty() {
-        let actual: Quad = vec![].iter().product();
-        assert_exact!(Quad::ONE, actual);
-    }
-
-    #[test]
-    fn product_inf() {
-        let actual: Quad = vec![qd!(1), Quad::INFINITY, qd!(3)].iter().product();
-        assert_exact!(Quad::INFINITY, actual);
-
-        let actual: Quad = vec![qd!(1), Quad::NEG_INFINITY, qd!(3)].iter().product();
-        assert_exact!(Quad::NEG_INFINITY, actual);
-
-        let actual: Quad = vec![Quad::INFINITY, Quad::NEG_INFINITY].iter().product();
-        assert_exact!(Quad::NEG_INFINITY, actual);
-    }
-
-    #[test]
-    fn product_nan() {
-        let actual: Quad = vec![qd!(1), qd!(2), Quad::NAN].iter().product();
-        assert_exact!(Quad::NAN, actual);
-    }
+    // product tests
+    test_all_near!(
+        product_nums_pi_234:
+            qd!("1.2919281950124925073115131277958914667593870235785461539226890876574"),
+            vec![Quad::FRAC_PI_2, Quad::FRAC_PI_3, Quad::FRAC_PI_4]
+                .into_iter()
+                .product::<Quad>();
+        product_refs_pi_234:
+            qd!("1.2919281950124925073115131277958914667593870235785461539226890876574"),
+            vec![Quad::FRAC_PI_2, Quad::FRAC_PI_3, Quad::FRAC_PI_4]
+                .iter()
+                .product::<Quad>();
+    );
+    test_all_exact!(
+        product_nums_15:
+            qd!(120),
+            vec![qd!(1), qd!(2), qd!(3), qd!(4), qd!(5)].into_iter().product::<Quad>();
+        product_refs_15:
+            qd!(120),
+            vec![qd!(1), qd!(2), qd!(3), qd!(4), qd!(5)].iter().product::<Quad>();
+        product_empty:
+            Quad::ONE,
+            vec![].iter().product::<Quad>();
+        product_inf:
+            Quad::INFINITY,
+            vec![qd!(1), Quad::INFINITY, qd!(3)].iter().product::<Quad>();
+        product_neg_inf:
+            Quad::NEG_INFINITY,
+            vec![qd!(1), Quad::NEG_INFINITY, qd!(3)].iter().product::<Quad>();
+        product_both_inf:
+            Quad::NEG_INFINITY,
+            vec![Quad::INFINITY, Quad::NEG_INFINITY].iter().product::<Quad>();
+        product_nan:
+            Quad::NAN,
+            vec![qd!(1), qd!(2), Quad::NAN].iter().product::<Quad>();
+    );
 }
